3edf4b47b7b8c37e4266a99d47fc2065f38bb59c
TomasM <tomas.mickus@ips.lt>
2022-05-31 17:36:09 +0300

    older hubs support added

--- Project/Middlewares/ST/STM32_USB_Host_Library/Class/HUB/usbh_hub.c
+++ Project/Middlewares/ST/STM32_USB_Host_Library/Class/HUB/usbh_hub.c
@@ -84,7 +84,10 @@ static USBH_StatusTypeDef USBH_HUB_InterfaceInit(USBH_HandleTypeDef *phost) {
         memset(&hUSBHost[h], 0, sizeof(USBH_HandleTypeDef));
     }
 
-    interface = USBH_FindInterface(phost, phost->pActiveClass->ClassCode, 0x00, 0x01);
+    interface = USBH_FindInterface(phost, phost->pActiveClass->ClassCode, 0x00, 0x00);
+    if(interface == 0xFF){
+        interface = USBH_FindInterface(phost, phost->pActiveClass->ClassCode, 0x00, 0x01);
+    }
 
     if (interface == 0xFF) /* No Valid Interface */
     {
